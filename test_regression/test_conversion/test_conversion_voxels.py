630ccd80d15eec72a858483a1986112328303b7f
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-11-20 19:28:56 +0400

    Add conversion tests on new point formats - unfinished

--- test_regression/test_conversion/test_conversion_voxels.py
+++ test_regression/test_conversion/test_conversion_voxels.py
@@ -5,21 +5,21 @@ from constants import test_files_path
 from helpers.meshlib_helpers import compare_voxels
 
 
-@pytest.mark.parametrize("ext", ["gav"])
+@pytest.mark.parametrize("ext", ["gav", "dcm", "vdb"])
 def test_conversion_from_raw(ext, tmp_path):
     filename_in = "W48_H58_S78_V1.5e+03_1.5e+03_1.5e+03_G0_F voxels.raw"
-    filename_out = "voxels.gav"
+    filename_out = f"voxels.{ext}"
     filename_ref = "voxels_ref.gav"
 
     input_folder = Path(test_files_path) / "conversion" / "voxels_from_raw"
 
     vxl_in = mrmeshpy.loadVoxelsRaw(input_folder / filename_in)
 
-    mrmeshpy.saveVoxelsGav(vxl_in, str(tmp_path / filename_out))
+    mrmeshpy.VoxelsSave.toAnySupportedFormat(vxl_in, str(tmp_path / filename_out))
 
     # Verify
-    vxl_ref =  mrmeshpy.loadVoxelsGav(input_folder / filename_ref)
-    vxl_out =  mrmeshpy.loadVoxelsGav(tmp_path / filename_out)
+    vxl_ref =  mrmeshpy.VoxelsLoad.fromAnySupportedFormat(input_folder / filename_ref)[0]
+    vxl_out =  mrmeshpy.VoxelsLoad.fromAnySupportedFormat(tmp_path / filename_out)[0]
 
     compare_voxels(vxl_out, vxl_ref)
 
